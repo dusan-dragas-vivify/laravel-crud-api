169955156398b7d519cdeea0a334842338c3c37f
dusan-dragas-vivify <dusan.dragas@vivifyideas.com>
2018-11-09 11:33:49 +0100

    user controller minor changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,26 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use App\Repositories\UserRepository;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    protected $userRepo;
+
+    public function __construct()
+    {
+        $this->userRepo = new UserRepository();
+    }
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
     {
         //
     }
@@ -34,7 +33,13 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $response = $this->userRepo->store($request);
+        if($response){
+            return response()->json([
+                'status' => http_response_code(200),
+                'message' => 'OK'
+            ]);
+        }
     }
 
     /**
@@ -44,17 +49,6 @@ class UserController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
     {
         //
     }
